000c10db4553b8f6f6d4f123c8a8fcdddcebbee9
Shahid <shahidpt0982219@gmail.com>
2021-10-20 20:37:07 +0530

    Added build/ to .gitignore file

--- client/build/static/js/main.bfec7450.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpapp=this.webpackJsonpapp||[]).push([[0],{11:function(e,n,t){"use strict";t.r(n);var c=t(1),i=t.n(c),p=t(3),r=t.n(p),s=(t(8),t(9),t(0));var a=function(){return Object(s.jsx)("div",{className:"App",children:Object(s.jsx)("h1",{children:"Hello ASKBNB client app"})})};r.a.render(Object(s.jsx)(i.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))},8:function(e,n,t){},9:function(e,n,t){}},[[11,1,2]]]);
-//# sourceMappingURL=main.bfec7450.chunk.js.map
